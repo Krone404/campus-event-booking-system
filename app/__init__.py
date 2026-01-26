da00b3211cf9761bcf4f0de2d140cc2b9d1a4b90
Cameron Cartwright <cameron.cartwright122804@gmail.com>
2026-01-26 21:46:57 +0000

    Allow create_app config overrides for unit tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask, redirect, url_for
+from typing import Optional, Dict, Any
 from .config import Config
 from .extensions import db, login_manager
 from .models import User
 from .security import csrf
 
-def create_app() -> Flask:
+def create_app(config_overrides: Optional[Dict[str, Any]] = None) -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    if config_overrides:
+        app.config.update(config_overrides)
 
     db.init_app(app)
     login_manager.init_app(app)
